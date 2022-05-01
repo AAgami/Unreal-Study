b4567f572010746711d5b751466bb0ed4594996f
hel00 <12345678910sla@gmail.com>
2022-05-01 18:57:23 +0900

    actor-fountain-upload

--- UE4CPP/ArenaBattle/Intermediate/Build/Win64/UE4/Inc/ArenaBattle/Fountain.gen.cpp
+++ UE4CPP/ArenaBattle/Intermediate/Build/Win64/UE4/Inc/ArenaBattle/Fountain.gen.cpp
@@ -17,6 +17,7 @@ void EmptyLinkFunctionForGeneratedCodeFountain() {}
 	ARENABATTLE_API UClass* Z_Construct_UClass_AFountain();
 	ENGINE_API UClass* Z_Construct_UClass_AActor();
 	UPackage* Z_Construct_UPackage__Script_ArenaBattle();
+	ENGINE_API UClass* Z_Construct_UClass_UStaticMeshComponent_NoRegister();
 // End Cross Module References
 	void AFountain::StaticRegisterNativesAFountain()
 	{
@@ -40,6 +41,24 @@ void EmptyLinkFunctionForGeneratedCodeFountain() {}
 				{ "ModuleRelativePath", "Fountain.h" },
 			};
 #endif
+#if WITH_METADATA
+			static const UE4CodeGen_Private::FMetaDataPairParam NewProp_Water_MetaData[] = {
+				{ "EditInline", "true" },
+				{ "ModuleRelativePath", "Fountain.h" },
+			};
+#endif
+			static const UE4CodeGen_Private::FObjectPropertyParams NewProp_Water = { UE4CodeGen_Private::EPropertyClass::Object, "Water", RF_Public|RF_Transient|RF_MarkAsNative, 0x0010000000080008, 1, nullptr, STRUCT_OFFSET(AFountain, Water), Z_Construct_UClass_UStaticMeshComponent_NoRegister, METADATA_PARAMS(NewProp_Water_MetaData, ARRAY_COUNT(NewProp_Water_MetaData)) };
+#if WITH_METADATA
+			static const UE4CodeGen_Private::FMetaDataPairParam NewProp_Body_MetaData[] = {
+				{ "EditInline", "true" },
+				{ "ModuleRelativePath", "Fountain.h" },
+			};
+#endif
+			static const UE4CodeGen_Private::FObjectPropertyParams NewProp_Body = { UE4CodeGen_Private::EPropertyClass::Object, "Body", RF_Public|RF_Transient|RF_MarkAsNative, 0x0010000000080008, 1, nullptr, STRUCT_OFFSET(AFountain, Body), Z_Construct_UClass_UStaticMeshComponent_NoRegister, METADATA_PARAMS(NewProp_Body_MetaData, ARRAY_COUNT(NewProp_Body_MetaData)) };
+			static const UE4CodeGen_Private::FPropertyParamsBase* const PropPointers[] = {
+				(const UE4CodeGen_Private::FPropertyParamsBase*)&NewProp_Water,
+				(const UE4CodeGen_Private::FPropertyParamsBase*)&NewProp_Body,
+			};
 			static const FCppClassTypeInfoStatic StaticCppClassTypeInfo = {
 				TCppClassTypeTraits<AFountain>::IsAbstract,
 			};
@@ -48,7 +67,7 @@ void EmptyLinkFunctionForGeneratedCodeFountain() {}
 				DependentSingletons, ARRAY_COUNT(DependentSingletons),
 				0x00900080u,
 				nullptr, 0,
-				nullptr, 0,
+				PropPointers, ARRAY_COUNT(PropPointers),
 				nullptr,
 				&StaticCppClassTypeInfo,
 				nullptr, 0,
@@ -58,7 +77,7 @@ void EmptyLinkFunctionForGeneratedCodeFountain() {}
 		}
 		return OuterClass;
 	}
-	IMPLEMENT_CLASS(AFountain, 2290081772);
+	IMPLEMENT_CLASS(AFountain, 3183999029);
 	static FCompiledInDefer Z_CompiledInDefer_UClass_AFountain(Z_Construct_UClass_AFountain, &AFountain::StaticClass, TEXT("/Script/ArenaBattle"), TEXT("AFountain"), false, nullptr, nullptr, nullptr);
 	DEFINE_VTABLE_PTR_HELPER_CTOR(AFountain);
 PRAGMA_ENABLE_DEPRECATION_WARNINGS
